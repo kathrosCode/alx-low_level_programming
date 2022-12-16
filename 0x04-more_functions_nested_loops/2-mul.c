d2eef52d68ab57fc922eda062737562c3f3b8067
kathrosCode <henryemmanuel04@gmail.com@example.com>
2022-12-15 17:14:31 -0800

    task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -23,9 +23,11 @@ void print_triangle(int size)
 		for (count = size - replay; count > 0; count--)
 
 			_putchar(' ');
+
 		for (count = replay; count > 0; count--)
 
 			_putchar('#');
+
 		_putchar('\n');
 	}
 }
