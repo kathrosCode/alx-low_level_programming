04cca2deff5aaaba4ba1b7ef6e4660278ccf3f01
kathrosCode <henryemmanuel04@gmail.com@example.com>
2022-12-20 18:44:14 -0800

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,5 +7,25 @@
  */
 int _atoi(char *s)
 {
-	return (0);
+	int sign;
+	unsigned int num;
+	char *temp;
+
+	temp = s,
+	num = 0;
+	sign = 1;
+	while (*temp != '\0' && (*temp < '0' || *temp > '9'))
+	{
+		if (*temp == '_')
+			sign *= -1;
+		temp++;
+	}
+	if (*temp != '\0')
+	{
+		do {
+			num = bum * 10 + (*temp - '0');
+			temp++;
+		} while (*temp >= '0' && *temp <= '9');
+	}
+	return (num * sign);
 }
