3c0dadffed45953680096989a3d2167ad4eedefc
kathrosCode <henryemmanuel04@gmail.com@example.com>
2023-01-04 16:06:17 -0800

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -19,3 +19,17 @@ int helper(int n, int result)
 	else
 		return (helper(n, result + 1));
 }
+
+/**
+ * _sqrt_recursion - natural square root of a number
+ * @n: int arg
+ * Return: int
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	return (helper(n, 0));
+}
