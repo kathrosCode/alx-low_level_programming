ff9cbb129f70edcac94c6c35d5b5e5e11f1038d7
kathrosCode <henryemmanuel04@gmail.com@example.com>
2023-01-22 17:05:59 -0800

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,19 +15,18 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	char *str;
-
-	va_start(numbers, n);
-
+	
+	
 	if (separator == NULL)
 	{
 		separator = "";
 	}
 
-	va_start(string, n);
+	va_start(strings, n);
 
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(string, char *);
+		str = va_arg(strings, char *);
 		if (str == NULL)
 		{
 			printf("(nil)");
@@ -41,5 +40,5 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		printf("%s", separator);
 	}
 	printf("\n");
-	va_end(string);
+	va_end(strings);
 }
